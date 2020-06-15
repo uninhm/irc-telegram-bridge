30e5f7c9db9c6c90bf3e308f85f699e85337441a
Unai Hernández Minaberry <unihernandez22@gmail.com>
2020-06-15 19:14:53 -0300

    Conexión doble

--- src/bot_irc.nim
+++ src/bot_irc.nim
@@ -1,22 +1,49 @@
-import irc, strutils
-var client = newIrc("irc.twitch.tv", nick="TestBotVicfred",
-                 joinChans = @["#vicfred"], serverPass = readFile("secret.key"))
-client.connect()
-while true:
-  var event: IrcEvent
-  if client.poll(event):
-    case event.typ
-    of EvConnected:
-      discard
-    of EvDisconnected, EvTimeout:
-      break
-    of EvMsg:
-      if event.cmd == MPrivMsg:
-        var msg = event.params[event.params.high]
-        if msg == "!test":
-          client.privmsg(event.origin, "hello")
-        elif "hola" in msg.normalize:
-          client.privmsg(event.origin, "Hola $1!" % @[event.nick])
-        elif msg == "!lag":
-          client.privmsg(event.origin, formatFloat(client.getLag))
-        echo msg
+import irc, strutils, telebot, strformat, asyncdispatch, options
+
+let TELEGRAM_TOKEN = readFile("telegram.token").strip
+var bot {.threadvar.}: TeleBot
+bot = newTeleBot(TELEGRAM_TOKEN)
+
+const GROUP_ID = -1001452904118
+
+proc onIrcEvent(client: AsyncIrc, event: IrcEvent) {.async.} =
+  case event.typ
+  of EvConnected:
+    discard nil
+  of EvDisconnected, EvTimeout:
+    await client.reconnect()
+  of EvMsg:
+    if event.cmd == MPrivMsg:
+      var msg = event.params[event.params.high]
+      if msg == "!test":
+        await client.privmsg(event.origin, "hello")
+      elif "hola" in msg.normalize:
+        await client.privmsg(event.origin, fmt"Hola {event.nick}!")
+      elif msg == "!lag":
+        await client.privmsg(event.origin, formatFloat(client.getLag))
+      if msg == "!users":
+        await client.privmsg(event.origin, "Users: " &
+            client.getUserList(event.origin).join("A-A"))
+      let text = fmt"{event.nick}: {msg}"
+      discard await bot.sendMessage(GROUP_ID, text)
+      echo text
+
+var client {.threadvar.}: AsyncIrc
+client = newAsyncIrc("irc.twitch.tv", nick="TestBotVicfred",
+                      joinChans = @["#vicfred"],
+                      serverPass = readFile("twitch.token"),
+                      callback = onIrcEvent)
+
+proc updateHandler(b: Telebot, u: Update): Future[bool] {.async.} =
+  if not u.message:
+    return true
+  var m = u.message.get
+  if m.text:
+    await client.privmsg("#vicfred", fmt"{m.fromUser.get.firstName}: {m.text.get}")
+
+
+bot.onUpdate(updateHandler)
+
+asyncCheck client.run()
+discard bot.pollAsync()
+runForever()
