f764b5d490d46da828f16170cba19bc82a3cb8bc
Unai Hernández Minaberry <unihernandez22@gmail.com>
2020-06-16 20:57:59 -0300

    Main file

--- src/bot_irc.nim
+++ src/bot_irc.nim
@@ -36,13 +36,6 @@ proc onIrcEvent(client: AsyncIrc, event: IrcEvent) {.async.} =
   of EvMsg:
     if event.cmd == MPrivMsg:
       var msg = event.params[event.params.high]
-      if "hola" in msg.normalize:
-        await client.privmsg(event.origin, fmt"Hola {event.nick}!")
-      elif msg == "!lag":
-        await client.privmsg(event.origin, formatFloat(client.getLag))
-      if msg == "!users":
-        await client.privmsg(event.origin, "Users: " &
-            client.getUserList(event.origin).join("A-A"))
       let text = fmt"*{event.nick}*: {msg}"
       discard await bot.sendMessage(TG_GROUP_ID, text, parseMode = "markdown")
 
